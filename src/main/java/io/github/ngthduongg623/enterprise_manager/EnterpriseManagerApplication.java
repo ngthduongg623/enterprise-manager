5f51c28d35de1b779126037103d8570d15208506
ngthduongg623 <ngthduong623@gmail.com>
2025-12-26 19:52:10 +0700

    Initial commit

---
+++ src/main/java/io/github/ngthduongg623/enterprise_manager/EnterpriseManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.ngthduongg623.enterprise_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ngthduongg623/enterprise_manager/EnterpriseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ngthduongg623.enterprise_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
